3dfa19c8aad503dc7652d9ddd943fd06178af3f0
PraveenX812 <praveenrawat784@gmail.com>
2025-12-30 00:51:04 +0530

    auth gateway complete

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
+    // loads .env into process.env -> simpler and also standard
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // connect our gateway to our mongo users
+    MongooseModule.forRoot(process.env.MONGO_URI_USERS as string),
+
+    UsersModule,
+
+    AuthModule,
+
     ClientsModule.register([
       {
         name: 'CATALOG_CLIENT',
@@ -12,20 +27,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         options: {
           urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
           queue: process.env.CATALOG_QUEUE ?? 'catalog_queue',
-          queueOptions: {
-            durable: false,
-          },
-        },
-      },
-      {
-        name: 'SEARCH_CLIENT',
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
-          queue: process.env.SEARCH_QUEUE ?? 'search_queue',
-          queueOptions: {
-            durable: false,
-          },
+          queueOptions: { durable: false },
         },
       },
       {
@@ -34,14 +36,23 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         options: {
           urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
           queue: process.env.MEDIA_QUEUE ?? 'media_queue',
-          queueOptions: {
-            durable: false,
-          },
+          queueOptions: { durable: false },
+        },
+      },
+      {
+        name: 'SEARCH_CLIENT',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
+          queue: process.env.SEARCH_QUEUE ?? 'search_queue',
+          queueOptions: { durable: false },
         },
       },
     ]),
   ],
-  controllers: [GatewayController],
+  controllers: [
+    GatewayController,
+  ],
   providers: [GatewayService],
 })
 export class GatewayModule {}
